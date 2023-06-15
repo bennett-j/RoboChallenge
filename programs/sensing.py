b70e0e246d82c2e48d21ab44dfa95eaddb74218d
bennett-j <53816688+bennett-j@users.noreply.github.com>
2023-06-15 23:59:22 +0100

    Update programs with correction and clarification

--- programs/sensing.py
+++ programs/sensing.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env pybricks-micropython
 
+# Import things we need from libraries.
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
 
-# Initialize the EV3 Brick and beep to notify.
+# Initialise the EV3 Brick and beep to notify.
 ev3 = EV3Brick()
-ev3.speaker.beep
+ev3.speaker.beep()
 
-# Initialize the motors.
+# Initialise the motors.
 right_motor = Motor(Port.B)
 left_motor = Motor(Port.C)
 
-# Initialize the colour sensor.
+# Initialise the colour sensor.
 line_sensor = ColorSensor(Port.S3)
 
-# Initialize the drive base.
+# Create 'robot' by initialising the drive base with motors and dimensions.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-# Calculate the white/black threshold using the midpoint.
-BLACK = 8
-WHITE = 45
-threshold = (BLACK + WHITE)/2
+# Specify the reflectance of the line and the background and calculate the threshold using the midpoint.
+LINE_REF = 5
+BACKGROUND_REF = 50
+threshold = (LINE_REF + BACKGROUND_REF)/2
 
-# While the reflection is brighter than the threshold (i.e. white), drive forwards
+# While the reflection is brighter than the threshold (i.e. white), drive forwards.
 while line_sensor.reflection() > threshold:
     robot.drive(100, 0)
 
