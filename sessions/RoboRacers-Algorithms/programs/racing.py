5d6dd1a44591a1af39b88b99955cbca61249e836
James Bennett <53816688+bennett-j@users.noreply.github.com>
2025-10-12 23:31:56 +0100

    Initial algorithm development session with new structure

---
+++ sessions/RoboRacers-Algorithms/programs/racing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env pybricks-micropython
+
+# Import things we need from libraries
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+# Initialise the EV3 Brick and beep to notify.
+ev3 = EV3Brick()
+ev3.speaker.beep()
+
+# Initialise the motors.
+right_motor = Motor(Port.B)
+left_motor = Motor(Port.C)
+
+# Initialise the colour sensor.
+line_sensor = ColorSensor(Port.S3)
+
+# Create 'robot' by initialising the drive base with motors and dimensions.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Specify the reflectance of the line and the background and calculate the threshold using the midpoint.
+LINE_REF = 5
+BACKGROUND_REF = 50
+threshold = (LINE_REF + BACKGROUND_REF)/2
+
+# Write your code here!
